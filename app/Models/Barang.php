d9ad31cc92b11122471bd883249a3fae473404e9
cu911030-prog <cu911030@gmail.com>
2026-07-03 02:00:18 +0700

    db: add BarangSeeder with idempotent data and update Kategori/Satuan seeders

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -8,11 +8,11 @@ class Barang extends Model
 {
     // Tambahkan ini agar bisa simpan data dari form
     protected $fillable = [
-        'kode_barang', 
-        'nama_barang', 
-        'kategori_id', 
-        'satuan_id', 
-        'stok', 
+        'kode_barang',
+        'nama_barang',
+        'kategori_id',
+        'satuan_id',
+        'stok',
         'harga'
     ];
 }
--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Barang;
+use App\Models\Kategori;
+use App\Models\Satuan;
 
 class BarangSeeder extends Seeder
 {
@@ -12,14 +15,26 @@ class BarangSeeder extends Seeder
      */
     public function run(): void
     {
+        $kategoriIds = Kategori::pluck('id')->toArray();
+        $satuanIds = Satuan::pluck('id')->toArray();
+
+        if (empty($kategoriIds)) {
+            $kategoriIds = [1];
+        }
+
+        if (empty($satuanIds)) {
+            $satuanIds = [1];
+        }
+
         for ($i = 1; $i <= 10; $i++) {
-            \App\Models\Barang::create([
+            Barang::updateOrCreate([
                 'kode_barang' => 'BRG' . str_pad($i, 3, '0', STR_PAD_LEFT),
+            ], [
                 'nama_barang' => 'Barang Dummy ' . $i,
-                'kategori_id' => 1, 
-                'satuan_id'   => 1, 
-                'stok'        => 10,
-                'harga'       => 10000,
+                'kategori_id' => $kategoriIds[($i - 1) % count($kategoriIds)],
+                'satuan_id'   => $satuanIds[($i - 1) % count($satuanIds)],
+                'stok'        => 10 + $i,
+                'harga'       => 10000 + ($i * 500),
             ]);
         }
     }
